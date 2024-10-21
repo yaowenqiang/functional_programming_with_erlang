b6ac6be28390efba2b763b0e5805691c66f9c3a1
姚文强 <yaowenqiang111@163.com>
2024-10-21 23:32:01 +0800

    add simplify function demo

--- parse.erl
+++ parse.erl
@@ -11,10 +11,50 @@ parse([Ch|Rest]) when $a =< Ch andalso Ch =< $z ->
 get_while(P, [Ch|Rest]) ->
     case P(Ch) of
         true -> 
-                io:format("~n", Rest),
-                [Succeeds|Remainder] = get_while(P, Rest),
+                % io:format("~n", Rest),
+                {Succeeds,Remainder} = get_while(P, Rest),
                 {[Ch|Succeeds], Remainder};
         false -> {[], [Ch|Rest]}
     end;
 get_while(_P, []) ->
     {[], []}.
+
+
+zeroA({add, E, {num, 0}}) ->
+    E;
+zeroA({add, {num, 0}, E}) ->
+    E;
+zeroA(E) ->
+    E.
+
+mulO({mul, E, {num, 1}}) ->
+    E;
+mulO({mul, {num, 1}, E}) ->
+    E;
+mulO(E) ->
+    E.
+mulZ({mul, E, {num, 0}}) ->
+    {num, 0};
+mulZ({mul, {num, 0},_ }) ->
+    {num, 0};
+mulZ(E) ->
+    E.
+
+compose([]) ->
+    fun(E) -> E end;
+compose([Rule|Rules]) ->
+    fun(E) -> (compose(Rules))(Rule(E)) end.
+
+rules() ->
+    [fun zeroA/1, fun mulO/1, fun mulZ/1].
+
+simp(F, {add, E1, E2}) ->
+    F({add, simp(F, E1), simp(F, E2)});
+simp(F, {mul, E1, E2}) ->
+    F({mul, simp(F, E1), simp(F, E2)});
+simp(_, E) -> E .
+
+simplify(E) ->
+    simp(compose(rules()), E).
+
+% expr:print(expr:simplify(expr:expr2()).
